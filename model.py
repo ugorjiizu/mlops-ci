a06e95166156649617d4d3df0108fe051fb738ed
ugorjiizu <66518563+ugorjiizu@users.noreply.github.com>
2023-02-17 22:15:22 +0100

    Update model.py

--- model.py
+++ model.py
@@ -90,7 +90,7 @@ model.fit(tf.expand_dims(X_train, axis=-1), y_train, epochs=50)
 
 # Make and plot predictions for model_1
 y_preds = model.predict(X_test)
-# plot_predictions(train_data=X_train, train_labels=y_train,  test_data=X_test, test_labels=y_test,  predictions=y_preds)
+plot_predictions(train_data=X_train, train_labels=y_train,  test_data=X_test, test_labels=y_test,  predictions=y_preds)
 
 
 # Calculate model_1 metrics 
